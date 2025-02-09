ef12043cf5ce1cc2738952a77fe2dc62d877e2f4
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-09 23:04:44 +0200

    Delete country_state.py

--- country_state.py
+++
@@ -1,9 +0,0 @@
-from interfaces.state import State
-
-class CountryState(State):
-    def handle(self, context, user_input: str):
-        context.data["country"] = user_input
-        print(f"Средняя цена квартир в стране {user_input}: ...")
-
-    def next_state(self):
-        return "CityState"
