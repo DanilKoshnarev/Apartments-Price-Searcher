3728abd7e9eb441080aa0dcfef6884d11c7c69a0
DanilKoshnarev <146003208+DanilKoshnarev@users.noreply.github.com>
2025-02-09 23:04:05 +0200

    Delete container.py

--- container.py
+++
@@ -1,9 +0,0 @@
-class Container:
-    def __init__(self):
-        self.services = {}
-
-    def register(self, name, service):
-        self.services[name] = service
-
-    def resolve(self, name):
-        return self.services.get(name)
